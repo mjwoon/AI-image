748211494b706a496c386ee699971f5b2af24cbf
mjwoon <jwmoon@hanyang.ac.kr>
2024-11-19 19:57:43 +0900

    fix: ai api finally works

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -16,61 +16,28 @@ const openai = new OpenAI({
 });
 
 router.route("/").get((req, res) => {
-  res.send("Hello from DALL-E!!");
+  res.status(200).json({ message: "Hello from DALL-E!" });
 });
 
-router.route("/").post(async (req, res, next) => {
-  let imageURL = "";
-  let imageBinaryData = "";
-
+router.route("/").post(async (req, res) => {
   try {
-    /** @type {string} */
-    const PROMPT = req.body;
+    const { prompt } = req.body;
 
     const aiResponse = await openai.images.generate({
       model: "dall-e-3",
-      prompt: PROMPT,
+      prompt,
       n: 1,
       size: "1024x1024",
       response_format: "b64_json",
     });
 
-    const imgFileName = `sample-${Date.now()}.png`;
-    const imgFilePath = `./public/images/${imgFileName}`;
-
-    if (aiResponse == "url") {
-      imageURL = res.data[0].url;
-
-      axios({
-        url: imageURL,
-        responseType: "stream",
-      })
-        .then((res) => {
-          res.data
-            .pipe(fs.createWriteStream(imgFilePath))
-            .on("finish", () => {
-              console.log("Image saved successfully.");
-            })
-            .on("error", (error) => {
-              console.error("Error saving image:", err);
-            });
-        })
-        .catch((error) => {
-          console.error("Error downloading image:", err);
-        });
-    } else {
-      imageBinaryData = response.data[0].b64_json;
-      const buffer = Buffer.from(imageBinaryData, "base64");
-      fs.writeFileSync(imgFilePath, buffer);
-      imageURL = `/images/${imgFileName}`;
-    }
-
+    const image = aiResponse.data[0].b64_json;
     res.status(200).json({ photo: image });
   } catch (error) {
     console.error(error);
     res
       .status(500)
-      .json(error?.response?.data?.error?.message || "An error occurred");
+      .send(error?.response.data.error.message || "Something went wrong");
   }
 });
 
